c7369fa16cd9899e0bb0fceabbee31b1b4a04818
Irina Chelysheva <irina.chelysheva@ndm.ox.ac.uk>
2025-03-09 16:28:31 +0000

    Update ML_Python_day1_regression.py

--- ML_Python_day1_regression.py
+++ ML_Python_day1_regression.py
@@ -5,7 +5,7 @@ Machine Learning in Python with scikit-learn
 COVID dataset analysis (regression problem)
 Author: Irina Chelysheva
 
-Date: March 3rd, 2025
+Date: March 10th, 2025
 COVID dataset extracted from OurWorldInData (https://github.com/owid/covid-19-data)
 """
 
